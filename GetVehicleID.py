ee93b3c73df11f1f241ddf39e3c95f2cd700a9b7
HWeigo <746144870@qq.com>
2019-07-10 01:55:25 +0800

    剔除连续变道情况

--- GetVehicleID.py
+++ GetVehicleID.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # 变道车ID截取
 data_Lane7 = np.loadtxt('../NGSIM_i80_1stTimePeriod_Lane7.csv',delimiter=",",skiprows = 1 , usecols=(range(0,14)))
-data_Lane67 = np.loadtxt('../NGSIM_i80_1stTimePeriod_Lane67.csv',delimiter=',',skiprows=1,usecols=(range(0,14)))
+data_Lane67 = np.loadtxt('../NGSIM_i80_1stTimePeriod_Lane567.csv',delimiter=',',skiprows=1,usecols=(range(0,14)))
 
 print(data_Lane7)
 idx_auto = (data_Lane7[:,10] == 2) # 仅分类轿车
@@ -15,12 +15,16 @@ print("Number of origin merge vehicle:" + str(VehicleID_Merge_Ori.size))
 # np.savetxt('../NGSIM_I80_1stTimePeriod_MergeVehicleID_Ori.csv',VehicleID_Merge_Ori, delimiter=',')
 
 
-
-# idx = (data_Lane67[:,0] == 46)
+# idx = (data_Lane67[:, 0] == 186)
 # vehicle_frame = data_Lane67[idx,1]-data_Lane67[idx,1].min() # 帧id从0计数
 # vehicle_x = data_Lane67[idx,4]
 # vehicle_y = data_Lane67[idx,5]
 # vehicle_lane = data_Lane67[idx,13]
+# print(vehicle_frame.size)
+# print(vehicle_y.size)
+# print(vehicle_lane.size)
+#
+# print(vehicle_frame)
 # for frame in vehicle_frame:
 #     frame_int = int(frame)
 #     print(frame_int)
@@ -32,9 +36,12 @@ print("Number of origin merge vehicle:" + str(VehicleID_Merge_Ori.size))
 # print(vehicle_frame[0])
 # tmp =int(vehicle_frame[0])
 # print(vehicle_y[tmp])
-#print(new)
+# #print(new)
+
+# NGSIM_I80_1stTimePeriod_Lane67数据异常，误用！
 
 # 剔除误记入汇流的数据
+last_lane = 7
 VehicleID_Merge = VehicleID_Merge_Ori
 for vehicle_ID in VehicleID_Merge_Ori:
     # print(vehicle_ID)
@@ -42,21 +49,37 @@ for vehicle_ID in VehicleID_Merge_Ori:
     vehicle_frame = data_Lane67[idx, 1] - data_Lane67[idx, 1].min()  # 帧id从0计数
     vehicle_x = data_Lane67[idx, 4]
     vehicle_y = data_Lane67[idx, 5]
-    # vehicle_lane = data_Lane67[idx,13]
+    vehicle_lane = data_Lane67[idx, 13]
 
     for frame in vehicle_frame:
-        if vehicle_y[int(frame)] > 410:   # 270
-            break
-        elif vehicle_x[int(frame)] < 80:  # 75
-            print('Delete'+str(vehicle_ID))
+        lane = vehicle_lane[int(frame)]
+
+        # 剔除借道车辆
+        if (vehicle_y[int(frame)] < 410) and (vehicle_x[int(frame)] < 80):  # 270
+            print('Delete Vehicle_ID: ' + str(vehicle_ID))
             VehicleID_Merge = np.delete(VehicleID_Merge, np.where(VehicleID_Merge == vehicle_ID))
             break
+
+        # 剔除连续变道车辆
+        if (last_lane == 7) & (lane == 6):
+            mergePoint_y = vehicle_y[int(frame)]
+            mergePoint_frame = frame
+            # print("Vehicle " + str(vehicle_ID) + "change to lane 6.")
+        if (last_lane == 6) & (lane == 5):
+            leavePoint_y = vehicle_y[int(frame)]
+            leavePoint_frame = frame
+            print("Vehicle " + str(vehicle_ID) + "change to lane 5.")
+            if (leavePoint_y - mergePoint_y < 70 ) or (leavePoint_frame - mergePoint_frame <40):
+                print('Delete Vehicle_ID: (Change from 7 directly to 5) ' + str(vehicle_ID))
+                VehicleID_Merge = np.delete(VehicleID_Merge, np.where(VehicleID_Merge == vehicle_ID))
+                break
+        last_lane = lane
 
 print(VehicleID_Merge)
 
 # 剔除连续变道
 
-np.savetxt('../NGSIM_I80_1stTimePeriod_MergeVehicleID_new.csv',VehicleID_Merge, delimiter=',')
+np.savetxt('../NGSIM_I80_1stTimePeriod_MergeVehicleID_rejectContinuousLaneChange.csv',VehicleID_Merge, delimiter=',')
 
 
 # idx = (data_Lane7[:,0] == 5)
--- lane7Trajectory.py
+++ lane7Trajectory.py
@@ -2,26 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-VehicleID_Merge = np.loadtxt('../NGSIM_I80_1stTimePeriod_MergeVehicleID_new.csv',delimiter=",")
-data_lane67 = np.loadtxt('../NGSIM_i80_1stTimePeriod_Lane67.csv',delimiter=',',skiprows=1,usecols=(range(0,14)))
-# VehicleID_Merge = np.delete(VehicleID_Merge, np.where(VehicleID_Merge == 355))
-VehicleID_Merge = np.delete(VehicleID_Merge, np.where(VehicleID_Merge == 1137))
-# VehicleID_Merge = np.delete(VehicleID_Merge, np.where(VehicleID_Merge == 2055))
+VehicleID_Merge = np.loadtxt('../NGSIM_I80_1stTimePeriod_MergeVehicleID_2rejectContinuousLaneChange.csv',delimiter=",")
+data_lane67 = np.loadtxt('../NGSIM_i80_1stTimePeriod_Lane567.csv',delimiter=',',skiprows=1,usecols=(range(0,14)))
 print(VehicleID_Merge)
 # print(data_lane67)
 
 fig = plt.figure('Scatter Fig')
 ax1 = fig.add_subplot(111)
 
-xLimit = [100, 1000]
+xLimit = [100, 930]
 ax1.set_title('Merging trajectory')
 plt.xlabel('local_y')
 plt.ylabel('local_x')
 plt.xlim(xLimit[0], xLimit[1])
 plt.ylim(100, 0)
 
-plt.plot(xLimit, [60.422, 60.422], color='black')
-plt.plot(xLimit, [74.421, 74.421], color='black')
+plt.plot(xLimit, [47.699, 47.699], color='black',linestyle="--")
+plt.plot(xLimit, [60.422, 60.422], color='black',linestyle="--")
+plt.plot(xLimit, [74.421, 74.421], color='black',linestyle="--")
+plt.plot([859, 357.43], [74.421, 100], color='black',linestyle="--")
+plt.annotate('...', (210,33))
+plt.annotate('Lane 4', (200,41))
+plt.annotate('Lane 5', (200,55))
+plt.annotate('Lane 6', (200,68.4))
+plt.annotate('Lane 7', (200,81))
+
 for vehicleID in VehicleID_Merge:
     idx = (data_lane67[:, 0] == vehicleID)
     local_x = data_lane67[idx, 4]
