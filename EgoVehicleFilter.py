2e6185371a79a6beffc49f43e881744e1b575c1d
Wei Huang <hweigo@hotmail.com>
2019-07-18 16:46:28 +0800

    提取成功汇流主旁车ID

---
+++ EgoVehicleFilter.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+VehicleID_Merge = np.loadtxt('../NGSIM_I80_1stTimePeriod_MergeVehicleID_2rejectContinuousLaneChange.csv',delimiter=",")
+data_lane567 = np.loadtxt('../NGSIM_i80_1stTimePeriod_Lane567_simplification.csv',delimiter=',',skiprows=1,usecols=(range(0,17)))
+
+last_lane = 7
+VehicleID_Ego_list = []
+for vehicleID in VehicleID_Merge:
+    idx = (data_lane567[:, 0] == vehicleID)
+    vehicle_frame = data_lane567[idx, 1] - data_lane567[idx, 1].min()  # 帧id从0计数
+    vehicle_lane = data_lane567[idx, 13]
+    PrecedingVehicle = data_lane567[idx, 14]
+    FollowingVehicle = data_lane567[idx, 15]
+
+    isFirstCount = 0
+    for frame in vehicle_frame:
+        lane = vehicle_lane[int(frame)]
+        if lane == 6 and last_lane == 7:
+            if isFirstCount == 0:
+                isFirstCount = 1
+                if FollowingVehicle[int(frame)] != 0:
+                    VehicleID_Ego_list.append(FollowingVehicle[int(frame)])
+                else:
+                    print("Vehicle " + str(vehicleID) + " didn't encounter ego vehicle.")
+                    VehicleID_Merge = np.delete(VehicleID_Merge, np.where(VehicleID_Merge == vehicleID))
+            else:
+                print("Vehicle " + str(vehicleID) + " merged morn than one time.")
+        last_lane = lane
+
+print(VehicleID_Ego_list)
+
+VehicleID_Ego = np.array(VehicleID_Ego_list)
+# VehicleID_Merge_list = VehicleID_Merge.tolist()
+
+print(type(VehicleID_Ego))
+print(type(VehicleID_Merge))
+
+VehicleID = np.empty((VehicleID_Ego.size, 2))
+print(VehicleID.shape)
+for vehicle in range(VehicleID_Ego.size):
+    VehicleID[vehicle][0] = VehicleID_Merge[vehicle]
+    VehicleID[vehicle][1] = VehicleID_Ego[vehicle]
+
+print(VehicleID)
+
+np.savetxt('../NGSIM_i80_1stTimePeriod_MergeAndEgo.csv', VehicleID, delimiter=',')
--- lane7Trajectory.py
+++ lane7Trajectory.py
@@ -5,6 +5,7 @@ import pandas as pd
 VehicleID_Merge = np.loadtxt('../NGSIM_I80_1stTimePeriod_MergeVehicleID_2rejectContinuousLaneChange.csv',delimiter=",")
 data_lane67 = np.loadtxt('../NGSIM_i80_1stTimePeriod_Lane567.csv',delimiter=',',skiprows=1,usecols=(range(0,14)))
 print(VehicleID_Merge)
+print("Number of merge vehicle: " + str(VehicleID_Merge.size))
 # print(data_lane67)
 
 fig = plt.figure('Scatter Fig')
@@ -26,11 +27,14 @@ plt.annotate('Lane 4', (200,41))
 plt.annotate('Lane 5', (200,55))
 plt.annotate('Lane 6', (200,68.4))
 plt.annotate('Lane 7', (200,81))
-
+# 显示图片时不暂停程序
+plt.ion()
 for vehicleID in VehicleID_Merge:
     idx = (data_lane67[:, 0] == vehicleID)
     local_x = data_lane67[idx, 4]
     local_y = data_lane67[idx, 5]
     ax1.scatter(local_y, local_x, s=1, alpha=0.3)
+    plt.show()
+    plt.pause(0.1)
 # plt.legend('x1')
-plt.show()
+
